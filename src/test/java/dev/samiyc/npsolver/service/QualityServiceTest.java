f550b1bb0ffe6837725598dbabc032a6caadc44f
Sami <sam-yc@hotmail.fr>
2024-04-14 23:41:18 +0200

    update test

--- src/test/java/dev/samiyc/npsolver/service/QualityServiceTest.java
+++ src/test/java/dev/samiyc/npsolver/service/QualityServiceTest.java
@@ -10,14 +10,14 @@ class EvaluationServiceUtilTest {
         Assertions.assertEquals(100, EvaluationStaticService.eval(10, 10, -6, 7));
         Assertions.assertEquals(100, EvaluationStaticService.eval(-10, -10, 5, 5));
         Assertions.assertEquals(100, EvaluationStaticService.eval(-10, -10, -5, 5));
-        Assertions.assertEquals(65, EvaluationStaticService.eval(10, 20, 2, 2));
-        Assertions.assertEquals(60, EvaluationStaticService.eval(-10, -30, -5, -5));
-        Assertions.assertEquals(55, EvaluationStaticService.eval(22, 23, 1, 1));
-        Assertions.assertEquals(45, EvaluationStaticService.eval(-14, 23, 1, 1));
-        Assertions.assertEquals(35, EvaluationStaticService.eval(-14, 23, 1, -1));
-        Assertions.assertEquals(20, EvaluationStaticService.eval(-14, 203, 2, -5));
-        Assertions.assertEquals(15, EvaluationStaticService.eval(-14, 203, 2, -51));
-        Assertions.assertEquals(5, EvaluationStaticService.eval(1000, -3, 1605, -21));
+        Assertions.assertEquals(90, EvaluationStaticService.eval(10, 20, 2, 2));
+        Assertions.assertEquals(90, EvaluationStaticService.eval(-10, -30, -5, -5));
+        Assertions.assertEquals(90, EvaluationStaticService.eval(22, 23, 1, 1));
+        Assertions.assertEquals(90, EvaluationStaticService.eval(-14, 23, 1, 1));
+        Assertions.assertEquals(70, EvaluationStaticService.eval(-14, 23, 1, -1));
+        Assertions.assertEquals(40, EvaluationStaticService.eval(-14, 203, 2, -5));
+        Assertions.assertEquals(30, EvaluationStaticService.eval(-14, 203, 2, -51));
+        Assertions.assertEquals(10, EvaluationStaticService.eval(1000, -3, 1605, -21));
         Assertions.assertEquals(0, EvaluationStaticService.eval(-1000, 31, -1602, 23));
 
         Assertions.assertEquals(100, EvaluationStaticService.eval(true, true));
